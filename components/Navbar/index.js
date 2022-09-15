23e31f94be8aac5cf173bfa7599acb0d01e8dd79
arkalal <arkalal.123@gmail.com>
2022-09-15 21:48:05 +0530

    changes CSS to SCSS

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import styles from "./Navbar.module.css";
+import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
   return (
